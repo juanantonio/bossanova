de34ae443c11d9c681b5a21ea7dd278907084632
juanantonio <juanantoniocr@gmail.com>
2012-09-05 16:05:15 +0200

    First commit from eclipse eGit

---
+++ ccresearchproxy/ccresearchproxy-0.0.1-SNAPSHOT/src/main/java/com/atos/research/ccresearchproxy/App.java
@@ -0,0 +1,13 @@
+package com.atos.research.ccresearchproxy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
